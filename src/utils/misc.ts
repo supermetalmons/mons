3e2b09d5ed894e4fe8a929cae645e2e26da88f6d
ivan grachev <ivan@ivan.lol>
2024-12-20 21:56:57 +0300

    fix brave broken on desktop

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -5,8 +5,8 @@ export const isDesktopSafari = (() => {
   return isSafari && !isIos;
 })();
 
-export const isMobile = /iPhone|iPad|iPod|Android|Windows Phone|IEMobile|Opera Mini/i.test(navigator.userAgent) || "brave" in navigator;
-export const isMobileOrVision = /iPhone|iPad|iPod|Android|Windows Phone|IEMobile|Opera Mini|visionOS/i.test(navigator.userAgent) || "brave" in navigator;
+export const isMobile = /iPhone|iPad|iPod|Android|Windows Phone|IEMobile|Opera Mini/i.test(navigator.userAgent);
+export const isMobileOrVision = /iPhone|iPad|iPod|Android|Windows Phone|IEMobile|Opera Mini|visionOS/i.test(navigator.userAgent);
 export const defaultInputEventName = isMobile ? "touchstart" : "click";
 
 export const isModernAndPowerful = (() => {
