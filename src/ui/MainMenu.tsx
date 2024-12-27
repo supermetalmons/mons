a13d26e62a36473a93c9944106ef8920b909bb6a
ivan grachev <ivan@ivan.lol>
2024-12-27 20:13:04 +0300

    hide eas checkmark for now

--- src/ui/MainMenu.tsx
+++ src/ui/MainMenu.tsx
@@ -529,7 +529,7 @@ const MainMenu: React.FC = () => {
           <RockMenu isOpen={isMenuOpen} showLeaderboard={showLeaderboard}>
             <MenuTitle onClick={!isMobile ? handleTitleClick : undefined} onTouchStart={isMobile ? handleTitleClick : undefined}>
               <MenuTitleText>MONS.LINK</MenuTitleText>
-              {showLeaderboard && (
+              {false && showLeaderboard && (
                 <EasLink href="https://base.easscan.org/schema/view/0x5c6e798cbb817442fa075e01b65d5d65d3ac35c2b05c1306e8771a1c8a3adb32" target="_blank" rel="noopener noreferrer">
                   ✓ EAS
                 </EasLink>
