bbf8aaf8f5755b29119fddf560b2f4ad5b143929
ivan grachev <ivan@ivan.lol>
2025-03-21 21:56:01 +0300

    put links buttons into a single row in the main menu

--- src/ui/MainMenu.tsx
+++ src/ui/MainMenu.tsx
@@ -178,6 +178,54 @@ const MenuTitle = styled.div`
   min-height: 20px;
 `;
 
+const IconLinkButton = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background-color: #f9f9f9;
+  color: #333;
+  text-decoration: none;
+  cursor: pointer;
+  -webkit-touch-callout: none;
+  touch-action: none;
+  user-select: none;
+  -webkit-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-tap-highlight-color: transparent;
+
+  @media (hover: hover) and (pointer: fine) {
+    &:hover {
+      background-color: #f5f5f5;
+    }
+  }
+
+  @media (prefers-color-scheme: dark) {
+    background-color: #252525;
+    color: #f5f5f5;
+
+    @media (hover: hover) and (pointer: fine) {
+      &:hover {
+        background-color: #272727;
+      }
+    }
+  }
+
+  svg {
+    width: 24px;
+    height: 24px;
+  }
+`;
+
+const ButtonRow = styled.div`
+  display: flex;
+  gap: 6px;
+  margin: 6px 16px;
+  align-items: center;
+`;
+
 const EasLink = styled.a`
   font-size: 12px;
   font-weight: 700;
@@ -239,109 +287,6 @@ const CloseButton = styled.button`
   @media (prefers-color-scheme: dark) {
     color: #424242;
     background: #232323;
-  }
-`;
-
-const IconRow = styled.div<{ hide: boolean }>`
-  display: flex;
-  justify-content: space-between;
-  gap: 6px;
-  margin-top: 9px;
-
-  opacity: ${(props) => (props.hide ? 0 : 1)};
-  height: ${(props) => (props.hide ? 0 : "auto")};
-  overflow: hidden;
-  transition: all 0.3s ease-in-out;
-
-  a {
-    color: #000;
-    text-decoration: none;
-    padding: 32px 16px;
-    border-radius: 8px;
-    text-align: left;
-    width: 75%;
-
-    @media (prefers-color-scheme: dark) {
-      color: #f5f5f5;
-    }
-  }
-`;
-
-const IconLink = styled.a`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 30px;
-  height: 30px;
-  border-radius: 50%;
-  background-color: #f9f9f9;
-
-  @media (hover: hover) and (pointer: fine) {
-    &:hover {
-      background-color: #f5f5f5;
-    }
-  }
-
-  @media (prefers-color-scheme: dark) {
-    background-color: #252525;
-
-    @media (hover: hover) and (pointer: fine) {
-      &:hover {
-        background-color: #272727;
-      }
-    }
-  }
-
-  img {
-    width: 24px;
-    height: 24px;
-  }
-`;
-
-const LinkRow = styled.div<{ hide: boolean }>`
-  display: flex;
-  justify-content: space-between;
-  gap: 6px;
-  align-items: center;
-  opacity: ${(props) => (props.hide ? 0 : 1)};
-  height: ${(props) => (props.hide ? 0 : "auto")};
-  overflow: hidden;
-  transition: all 0.3s ease-in-out;
-`;
-
-const LinkButton = styled.a`
-  flex: 1;
-  padding: 27px 0px;
-  text-align: center;
-  font-size: 0.81rem;
-  font-weight: 600;
-  border-radius: 8px;
-  background-color: #f9f9f9;
-  color: #333;
-  text-decoration: none;
-  cursor: pointer;
-  -webkit-touch-callout: none;
-  touch-action: none;
-  user-select: none;
-  -webkit-user-select: none;
-  -webkit-user-drag: none;
-  -webkit-tap-highlight-color: transparent;
-
-  @media (hover: hover) and (pointer: fine) {
-    &:hover {
-      background-color: #f5f5f5;
-    }
-  }
-
-  @media (prefers-color-scheme: dark) {
-    background-color: #252525;
-    color: #f5f5f5;
-
-    @media (hover: hover) and (pointer: fine) {
-      &:hover {
-        background-color: #272727;
-      }
-    }
   }
 `;
 
@@ -646,6 +591,27 @@ const MainMenu: React.FC = () => {
                 </EasLink>
               )}
             </MenuTitle>
+            <ButtonRow>
+              <IconLinkButton href="https://x.com/supermetalx" target="_blank" rel="noopener noreferrer">
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="currentColor">
+                  <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" stroke="currentColor" stroke-width="0.2" />
+                </svg>
+              </IconLinkButton>
+              <IconLinkButton href="https://github.com/supermetalmons" target="_blank" rel="noopener noreferrer">
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="currentColor">
+                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                </svg>
+              </IconLinkButton>
+              <IconLinkButton href="https://opensea.io/collection/supermetalmons" target="_blank" rel="noopener noreferrer">
+                Gen 1
+              </IconLinkButton>
+              <IconLinkButton href="https://opensea.io/collection/super-metal-mons-gen-2" target="_blank" rel="noopener noreferrer">
+                Gen 2
+              </IconLinkButton>
+              <IconLinkButton href="https://www.supermetalmons.com/collections/all" target="_blank" rel="noopener noreferrer">
+                IRL
+              </IconLinkButton>
+            </ButtonRow>
             <CloseButton
               onClick={() => {
                 setIsMenuOpen(false);
@@ -654,29 +620,6 @@ const MainMenu: React.FC = () => {
               ×
             </CloseButton>
             {showExperimental && <MenuOverlay />}
-            <IconRow hide={true}>
-              <IconLink href="https://x.com/supermetalx" target="_blank" rel="noopener noreferrer">
-                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="currentColor">
-                  <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" stroke="currentColor" stroke-width="0.2" />
-                </svg>
-              </IconLink>
-              <IconLink href="https://github.com/supermetalmons" target="_blank" rel="noopener noreferrer">
-                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="currentColor">
-                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-                </svg>
-              </IconLink>
-            </IconRow>
-            <LinkRow hide={true}>
-              <LinkButton href="https://opensea.io/collection/supermetalmons" target="_blank" rel="noopener noreferrer">
-                Gen 1
-              </LinkButton>
-              <LinkButton href="https://opensea.io/collection/super-metal-mons-gen-2" target="_blank" rel="noopener noreferrer">
-                Gen 2
-              </LinkButton>
-              <LinkButton href="https://www.supermetalmons.com/collections/all" target="_blank" rel="noopener noreferrer">
-                IRL
-              </LinkButton>
-            </LinkRow>
             <Leaderboard show={true} />
             {showExperimental && (
               <ExperimentalMenu>
