a5fb2a9bc24325e446ad696060e3361da0b4d070
ivan grachev <ivan@ivan.lol>
2025-05-08 12:44:12 +0300

    get eth nfts

--- cloud_functions/functions/getNfts.js
+++ cloud_functions/functions/getNfts.js
@@ -6,63 +6,99 @@ exports.getNfts = onCall(async (request) => {
     throw new HttpsError("unauthenticated", "The function must be called while authenticated.");
   }
 
-  const apiKey = process.env.HELIUS_API_KEY;
-  if (!apiKey) {
-    throw new HttpsError("failed-precondition", "Helius API key is not configured.");
-  }
-
   const sol = request.data.sol;
+  const eth = request.data.eth;
 
-  if (!sol) {
-    throw new HttpsError("invalid-argument", "Solana address is required.");
+  if (!sol && !eth) {
+    throw new HttpsError("invalid-argument", "Some address is required.");
   }
 
   try {
-    const response = await fetch("https://mainnet.helius-rpc.com/?api-key=" + apiKey, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        jsonrpc: "2.0",
-        id: "my-id",
-        method: "searchAssets",
-        params: {
-          ownerAddress: sol,
-          grouping: ["collection", "CjL5WpAmf4cMEEGwZGTfTDKWok9a92ykq9aLZrEK2D5H"],
-          page: 1,
-          limit: 50,
+    if (sol) {
+      const response = await fetch("https://mainnet.helius-rpc.com/?api-key=" + process.env.HELIUS_API_KEY, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-      }),
-    });
-
-    const data = await response.json();
-
-    if (!response.ok) {
-      throw new HttpsError("internal", "Failed to fetch NFTs from Helius API", data);
-    }
-
-    const filteredNfts =
-      data?.result?.items?.map((item) => ({
-        id: item.id,
-        content: {
-          json_uri: item.content?.json_uri || "",
-          links: item.content?.links,
-          metadata: {
-            name: item.content?.metadata?.name || "",
-            image: item.content?.metadata?.image,
+        body: JSON.stringify({
+          jsonrpc: "2.0",
+          id: "my-id",
+          method: "searchAssets",
+          params: {
+            ownerAddress: sol,
+            grouping: ["collection", "CjL5WpAmf4cMEEGwZGTfTDKWok9a92ykq9aLZrEK2D5H"],
+            page: 1,
+            limit: 50,
           },
-        },
-        ownership: {
-          owner: item.ownership?.owner || "",
-        },
-      })) || [];
+        }),
+      });
 
-    return {
-      ok: true,
-      nfts: filteredNfts,
-      total: data?.result?.total || 0,
-    };
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new HttpsError("internal", "Failed to fetch NFTs from Helius API", data);
+      }
+
+      const filteredNfts =
+        data?.result?.items?.map((item) => ({
+          id: item.id,
+          content: {
+            json_uri: item.content?.json_uri || "",
+            links: item.content?.links,
+            metadata: {
+              name: item.content?.metadata?.name || "",
+              image: item.content?.metadata?.image,
+            },
+          },
+          ownership: {
+            owner: item.ownership?.owner || "",
+          },
+        })) || [];
+
+      return {
+        ok: true,
+        nfts: filteredNfts,
+        total: data?.result?.total || 0,
+      };
+    } else if (eth) {
+      const response = await fetch(`https://api.opensea.io/api/v2/chain/ethereum/account/${eth}/nfts?collection=super-metal-mons-gen-2`, {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          "x-api-key": process.env.OPENSEA_API_KEY,
+        },
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new HttpsError("internal", "Failed to fetch NFTs from OpenSea API", data);
+      }
+
+      const filteredNfts =
+        data?.nfts?.map((item) => ({
+          id: item.identifier,
+          content: {
+            json_uri: item.metadata_url || "",
+            links: {
+              image: item.display_image_url,
+            },
+            metadata: {
+              name: item.name || "",
+              image: item.display_image_url,
+            },
+          },
+          ownership: {
+            owner: eth,
+          },
+        })) || [];
+
+      return {
+        ok: true,
+        nfts: filteredNfts,
+        total: data?.nfts?.length || 0,
+      };
+    }
   } catch (error) {
     console.error("Error fetching NFTs:", error);
     throw new HttpsError("internal", "Failed to fetch NFTs", error);
