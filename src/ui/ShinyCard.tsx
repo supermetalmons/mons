4a2ab7b63478de9145f48879e250968b30e2b559
ivan grachev <ivan@ivan.lol>
2025-05-16 21:50:31 +0300

    refactor showing stickers

--- src/ui/ShinyCard.tsx
+++ src/ui/ShinyCard.tsx
@@ -463,6 +463,9 @@ export const showShinyCard = async (profile: PlayerProfile | null, displayName: 
   observer.observe(document.body, { childList: true });
   showMons(cardContentsLayer, handlePointerLeave, isOtherPlayer, profile);
 
+  // TODO: show actual stickers, not random
+  // showRandomStickers(cardContentsLayer);
+
   updateUndoButton();
 };
 
@@ -476,24 +479,41 @@ export const updateShinyCardDisplayName = (displayName: string) => {
   }
 };
 
-async function showRandomStickers(cardContentsLayer: HTMLElement) {
-  const stickerPaths = {
-    "type-logo": ["spirit", "mystic", "drainer", "demon", "angel"],
-    "mini-logo": ["super-mana", "potion", "mana", "bomb"],
-    "middle-right": ["swag-coin", "metal-mana-pog", "glitter-rock"],
-    "middle-left": ["super-mana-piece-2", "super-mana-piece"],
-    mana: ["metal-mana", "blue-mana"],
-    "bottom-right": ["star", "cursor"],
-    "bottom-left": ["rock", "heart"],
-    "big-mon-top-right": ["zemred", "super-mana-piece-3", "speklmic", "omom-4", "omom-3", "omom-2", "omom", "omen-statue", "melmut", "lord-idgecreist", "king-snowbie", "hatchat", "gummy-deino", "gerp", "estalibur", "crystal-owg", "crystal-gummy-deino", "crystal-cloud-gabber", "armored-gummoskullj", "applecreme"],
-  };
+const STICKER_PATHS = {
+  "type-logo": ["spirit", "mystic", "drainer", "demon", "angel"],
+  "mini-logo": ["super-mana", "potion", "mana", "bomb"],
+  "middle-right": ["swag-coin", "metal-mana-pog", "glitter-rock"],
+  "middle-left": ["super-mana-piece-2", "super-mana-piece"],
+  mana: ["metal-mana", "blue-mana"],
+  "bottom-right": ["star", "cursor"],
+  "bottom-left": ["rock", "heart"],
+  "big-mon-top-right": ["zemred", "super-mana-piece-3", "speklmic", "omom-4", "omom-3", "omom-2", "omom", "omen-statue", "melmut", "lord-idgecreist", "king-snowbie", "hatchat", "gummy-deino", "gerp", "estalibur", "crystal-owg", "crystal-gummy-deino", "crystal-cloud-gabber", "armored-gummoskullj", "applecreme"],
+};
 
-  for (const [path, options] of Object.entries(stickerPaths)) {
+function getRandomStickers(): string {
+  const selectedStickers: Record<string, string> = {};
+
+  for (const [path, options] of Object.entries(STICKER_PATHS)) {
     const randomSticker = options[Math.floor(Math.random() * options.length)];
-    const stickerUrl = `https://assets.mons.link/cards/stickers/${path}/${randomSticker}.webp`;
+    selectedStickers[path] = randomSticker;
+  }
+
+  return JSON.stringify(selectedStickers);
+}
+
+function displayStickers(cardContentsLayer: HTMLElement, stickersJson: string) {
+  const selectedStickers = JSON.parse(stickersJson);
+
+  for (const [path, sticker] of Object.entries(selectedStickers)) {
+    const stickerUrl = `https://assets.mons.link/cards/stickers/${path}/${sticker}.webp`;
     const stickers = createOverlayImage(stickerUrl);
     cardContentsLayer.appendChild(stickers);
   }
+}
+
+async function showRandomStickers(cardContentsLayer: HTMLElement) {
+  const stickersJson = getRandomStickers();
+  displayStickers(cardContentsLayer, stickersJson);
 }
 
 const addImageToCard = (cardContentsLayer: HTMLElement, leftPosition: string, topPosition: string, imageData: string, alpha: number, monType: string = "", handlePointerLeave: any, isOtherPlayer: boolean): HTMLElement => {
