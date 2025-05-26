78415e559bc0136982c9585f93042992c206f704
ivan grachev <ivan@ivan.lol>
2025-05-26 19:33:47 +0300

    designated music cover

--- src/content/music.ts
+++ src/content/music.ts
@@ -1,22 +1,27 @@
-import { logoBase64 } from "../content/uiAssets";
 import { setIsMusicPlayingGlobal } from "../index";
 
 const tracks = ["arploop", "band", "bell-dance", "bell-glide", "bounce", "bubble-jam", "buzz", "change", "chimes-photography_going-home", "clock-tower", "cloud-propeller-2", "cloud-propeller", "crumbs", "driver", "drreams", "ewejam", "gilded", "gustofwind", "honkshoooo-memememeee-zzzZZZ", "jelly-jam", "mana-pool", "melodine", "object", "organwhawha", "ping", "runner", "spirit-track", "super", "whale2"];
 
 let audioElement: HTMLAudioElement | null = null;
 let currentTrack = "";
+let mediaMetadata: MediaMetadata | null = null;
 
 export function showMonsAlbumArtwork(title: string) {
-  if ("mediaSession" in navigator) {
-    navigator.mediaSession.metadata = new MediaMetadata({
-      title: title,
+  if (!mediaMetadata) {
+    mediaMetadata = new MediaMetadata({
       artist: "mons.link",
       artwork: [
         {
-          src: logoBase64,
+          src: "/assets/misc/cover.jpg",
+          sizes: "512x512",
         },
       ],
     });
+  }
+
+  if ("mediaSession" in navigator) {
+    mediaMetadata.title = title;
+    navigator.mediaSession.metadata = mediaMetadata;
   }
 }
 
