8a364f660cb22836527aa06341c506ce42bdb33a
ivan grachev <ivan@ivan.lol>
2025-07-21 20:57:03 +0300

    tune instructions outline

--- src/game/board.ts
+++ src/game/board.ts
@@ -153,8 +153,7 @@ export function showInstructionsText(text: string) {
     textDiv.style.textAlign = "left";
     textDiv.style.lineHeight = "1.2";
     textDiv.style.overflow = "visible";
-    textDiv.style.border = "2px solid var(--instruction-text-color)";
-    textDiv.style.borderRadius = "0.95em";
+    textDiv.style.border = "2px dotted var(--instruction-text-color)";
     textDiv.style.pointerEvents = "none";
     textDiv.style.touchAction = "none";
 
