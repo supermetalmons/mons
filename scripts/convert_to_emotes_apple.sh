35c9193a13b1416416f41c6d849a915854ca877d
ivan grachev <ivan@ivan.lol>
2025-08-12 10:38:31 +0300

    tune apple emotes conversion

--- scripts/convert_to_emotes_apple.sh
+++ scripts/convert_to_emotes_apple.sh
@@ -8,7 +8,11 @@ for f in ./*.mov; do
   name="${base%.*}"
   out="output/$name.mov"
   tmpdir="$(mktemp -d)"
-  ffmpeg -y -i "$f" -vf "scale=231:231:flags=bicubic" -c:v prores_ks -profile:v 4 -pix_fmt yuva444p10le -an "$tmpdir/resized.mov" && \
+  ffmpeg -y -i "$f" \
+    -vf "scale=231:231:flags=lanczos+accurate_rnd+full_chroma_int,format=yuva444p10le" \
+    -c:v prores_ks -profile:v 4 -pix_fmt yuva444p10le -an \
+    -color_primaries bt709 -color_trc bt709 -colorspace bt709 \
+    "$tmpdir/resized.mov" && \
   avconvert -s "$tmpdir/resized.mov" -o "$out" -p PresetHEVCHighestQualityWithAlpha --replace --progress
   rm -rf "$tmpdir"
 done
