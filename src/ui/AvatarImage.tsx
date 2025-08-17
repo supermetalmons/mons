15b6413bdb3f165fd08260c8cacb396090807cbc
ivan grachev <ivan@ivan.lol>
2025-08-17 14:56:25 +0300

    tune rainbow aura

--- src/ui/AvatarImage.tsx
+++ src/ui/AvatarImage.tsx
@@ -4,25 +4,11 @@ import { colors } from "../content/boardStyles";
 
 const r = colors.rainbow;
 
-const rainbowAura = keyframes`
-  0% { 
-    background: radial-gradient(circle at center, ${r[1]} 0%, ${r[2]} 20%, ${r[3]} 40%, ${r[4]} 60%, ${r[5]} 80%, ${r[6]} 100%);
+const rainbowRotation = keyframes`
+  0% {
     transform: rotate(0deg);
   }
-  25% { 
-    background: radial-gradient(circle at center, ${r[2]} 0%, ${r[3]} 20%, ${r[4]} 40%, ${r[5]} 60%, ${r[6]} 80%, ${r[7]} 100%);
-    transform: rotate(90deg);
-  }
-  50% { 
-    background: radial-gradient(circle at center, ${r[3]} 0%, ${r[4]} 20%, ${r[5]} 40%, ${r[6]} 60%, ${r[7]} 80%, ${r[1]} 100%);
-    transform: rotate(180deg);
-  }
-  75% { 
-    background: radial-gradient(circle at center, ${r[4]} 0%, ${r[5]} 20%, ${r[6]} 40%, ${r[7]} 60%, ${r[1]} 80%, ${r[2]} 100%);
-    transform: rotate(270deg);
-  }
-  100% { 
-    background: radial-gradient(circle at center, ${r[5]} 0%, ${r[6]} 20%, ${r[7]} 40%, ${r[1]} 60%, ${r[2]} 80%, ${r[3]} 100%);
+  100% {
     transform: rotate(360deg);
   }
 `;
@@ -42,11 +28,11 @@ const RainbowBackground = styled.div<{ hasRainbowAura: boolean }>`
   height: 110%;
   top: -5%;
   left: -5%;
-
   ${({ hasRainbowAura }) =>
     hasRainbowAura &&
     css`
-      animation: ${rainbowAura} 4s linear infinite;
+      background: conic-gradient(${r[1]} 0deg, ${r[2]} 51.428deg, ${r[3]} 102.857deg, ${r[4]} 154.286deg, ${r[5]} 205.714deg, ${r[6]} 257.143deg, ${r[7]} 308.571deg, ${r[1]} 360deg);
+      animation: ${rainbowRotation} 20s linear infinite;
       filter: blur(2px);
       opacity: 0.8;
     `}
