2d301c1bfdce74c0b34b7a2f09d0745bba7f7a05
ivan grachev <ivan@ivan.lol>
2025-08-17 21:11:23 +0300

    lint

--- src/ui/AvatarImage.tsx
+++ src/ui/AvatarImage.tsx
@@ -31,7 +31,7 @@ const RainbowBackground = styled.div<{ hasRainbowAura: boolean }>`
   ${({ hasRainbowAura }) =>
     hasRainbowAura &&
     css`
-      background: conic-gradient(${r[1]} 0deg, ${r[2]} 45deg, ${r[3]} 90deg, ${r[4]} 135deg, ${r[5]} 180deg, ${r[6]} 225deg, #0066FF 270deg, ${r[7]} 315deg, ${r[1]} 360deg);
+      background: conic-gradient(${r[1]} 0deg, ${r[2]} 45deg, ${r[3]} 90deg, ${r[4]} 135deg, ${r[5]} 180deg, ${r[6]} 225deg, #0066ff 270deg, ${r[7]} 315deg, ${r[1]} 360deg);
       animation: ${rainbowRotation} 20s linear infinite;
       filter: blur(2px);
       opacity: 0.8;
